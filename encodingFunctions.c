a3ba63bb7910a7fe7ae0d165b62fedf99aa3b20f
deak-s <sophia.deak@gmail.com>
2020-02-12 13:18:58 -0500

    unidentified changes

--- encodingFunctions.c
+++ encodingFunctions.c
@@ -12,50 +12,49 @@
 #include "conversionFunctions.h"
 
 int encodeArray(char input[], char output[]){
-   
-   int tritCounter = 0; 
-   int x;
+
+    int tritCounter = 0; 
+    int x;
     //iterate through char array
     //use to be (int)input[x] != 0
-      for(x = 0; (int)input[x] != 0; x++){
+    for(x = 0; (int)input[x] != 0; x++){
 
-          int t = howManyTrit((int)input[x]);
+        int t = howManyTrit((int)input[x]);
 
-          int *tritArray = decToBase3Array((int)input[x]); 
-
-
-          //first trit to output array
-          //assumes prev element was 'A'
-          if(tritCounter == 0){
-              output[0] = encodeTrit( 'A', tritArray[0]);
-              tritCounter += 1;
-
-              for(int k = 1; k <t; k++){
-                  output[tritCounter] = encodeTrit(output[tritCounter - 1], tritArray[k]);
-                  tritCounter += 1;
-              }
-          }
-
-          //now have trit array repping one input char
-          //must iterate through trit[] and swap to base pair
-          else {
-              for(int k = 0; k < t; k++){
+        int *tritArray = decToBase3Array((int)input[x]); 
 
 
-                  output[tritCounter] = encodeTrit(output[tritCounter - 1], tritArray[k]);
+        //first trit to output array
+        //assumes prev element was 'A'
+        if(tritCounter == 0){
+            output[0] = encodeTrit( 'A', tritArray[0]);
+            tritCounter += 1;
 
-                  tritCounter += 1;
-              }
-          }
-           }
+            for(int k = 1; k <t; k++){
+                output[tritCounter] = encodeTrit(output[tritCounter - 1], tritArray[k]);
+                tritCounter += 1;
+            }
+        }
 
-      output[tritCounter + 1] = '\0';
+        //now have trit array repping one input char
+        //must iterate through trit[] and swap to base pair
+        else {
+            for(int k = 0; k < t; k++){
 
-/*
-      for (int j = 0 ; j < tritCounter; j++){
-          printf("%c", output[j]);
-      }*/
-//      printf("results:\n%d characters encoded\n%d bases\n", x, tritCounter);
+                output[tritCounter] = encodeTrit(output[tritCounter - 1], tritArray[k]);
+
+                tritCounter += 1;
+            }
+        }
+    }
+
+    output[tritCounter + 1] = '\0';
+
+    /*
+       for (int j = 0 ; j < tritCounter; j++){
+       printf("%c", output[j]);
+       }*/
+    //      printf("results:\n%d characters encoded\n%d bases\n", x, tritCounter);
     return tritCounter;
 }
 
